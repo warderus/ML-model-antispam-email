9049715731e799a9d4c955742e2f10a02406f516
Warderus <varderus@yandex.ru>
2023-03-22 00:58:03 -0400

    add ipynb

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from transformers import BertTokenizer, BertForSequenceClassification
 from flask import Flask, jsonify, request
 
-# Вопрос в каком виде импортировать модель через метод from_pretrained()
+
 tokenizer = BertTokenizer.from_pretrained('model_spam')
 model =BertForSequenceClassification.from_pretrained('model_spam')
 
@@ -14,8 +14,7 @@ app = Flask(__name__)
 @app.route("/antispam", methods=["POST"])
 def antispam():
     X = request.get_json()
-    # Вопрос в каком виде и каким методом отправлять данные в модель
-    preds = model.predict_proba(pd.DataFrame(X, index=[0]))[0, 1]
+    preds = model.predict_proba(pd.DataFrame(X, index=[0]))
     result = {"answer": preds}
     return jsonify(result)
 
